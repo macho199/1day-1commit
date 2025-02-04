20105c0348fcbd6c89076e71f11de05f3b379b9c
macho199 <macho199@gmail.com>
2025-02-05 00:00:31 +0900

    add first commit

---
+++ 20250204-Display-API-for-bulletin-boards-created-by-springboot/board/src/main/java/com/macho199/board/controller/BoardController.java
@@ -0,0 +1,15 @@
+package com.macho199.board.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class BoardController {
+
+    @GetMapping
+    public String index() {
+        return "Hello World";
+    }
+}
